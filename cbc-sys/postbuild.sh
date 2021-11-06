fa58e7cf524b8c391d0a5ee31ea06b647b24e17a
Gregory Arefyev <grinya.guitar@gmail.com>
2021-11-06 19:23:12 +0100

    Coin-OR CBC solver

---
+++ cbc-sys/postbuild.sh
@@ -0,0 +1,1 @@
+[ -e coinor ] && rm -rf coinor
---
+++ cbc-sys/prebuild.sh
@@ -0,0 +1,11 @@
+[ -e coinor ] && rm -rf coinor
+mkdir -p coinor
+cd coinor
+
+git clone https://github.com/coin-or/Cbc.git --branch releases/2.10.5
+git clone https://github.com/coin-or/Cgl.git --branch releases/0.60.3
+git clone https://github.com/coin-or/Clp.git --branch releases/1.17.6
+git clone https://github.com/coin-or/CoinUtils.git --branch releases/2.11.4
+git clone https://github.com/coin-or/Osi.git --branch releases/0.108.6
+
+cd Cbc && git apply ../../cbc.patch
