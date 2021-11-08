7585d260f4da913093308727c5ee499a206500fb
Gregory Arefyev <grinya.guitar@gmail.com>
2021-11-08 01:41:01 +0100

    Set integer vars in bulk

--- cbc-sys/prebuild.sh
+++ cbc-sys/prebuild.sh
@@ -9,3 +9,4 @@ git clone https://github.com/coin-or/CoinUtils.git --branch releases/2.11.4
 git clone https://github.com/coin-or/Osi.git --branch releases/0.108.6
 
 cd Cbc && git apply ../../cbc.patch
+cd ../Clp && git apply ../../clp.patch
